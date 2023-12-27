3c2f86f3305919bb708a5520192926a8e910e073
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-28 02:48:34 +0700

    add Icon

--- app/src/main/java/com/example/consrestapi/ui/KontakApp.kt
+++ app/src/main/java/com/example/consrestapi/ui/KontakApp.kt
@@ -2,8 +2,11 @@ package com.example.consrestapi.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -72,9 +75,10 @@ fun TopAppBarKontak(
         scrollBehavior = scrollBehavior,
         navigationIcon = {
             if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
-
-                }
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = ""
+                )
             }
         })
 }
