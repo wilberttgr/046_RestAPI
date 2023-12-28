a3dfb998bd423b8e3d99d124511f45d73d02a0a7
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-28 18:21:51 +0700

    menambahkan override suspend fun delete and insert

--- app/src/main/java/com/example/consrestapi/repository/KontakRepository.kt
+++ app/src/main/java/com/example/consrestapi/repository/KontakRepository.kt
@@ -5,10 +5,27 @@ import com.example.consrestapi.service_api.KontakService
 
 interface KontakRepository{
     suspend fun getKontak(): List<Kontak>
+    suspend fun deleteKontak(id: Int): Boolean
+    suspend fun insertKontak(kontak: Kontak): Boolean
 }
 
 class NetworkKontakRepository(
     private val kontakApiService: KontakService
 ) : KontakRepository{
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+    override suspend fun deleteKontak(id: Int): Boolean {
+        // Implementasi penghapusan kontak melalui kontakApiService dengan menggunakan id
+        val response = kontakApiService.deleteKontak(id)
+
+        // Mengembalikan nilai true jika penghapusan berhasil, false sebaliknya
+        return response.isSuccessful
+    }
+
+    override suspend fun insertKontak(kontak: Kontak): Boolean {
+        // Implementasi penambahan kontak melalui kontakApiService
+        val response = kontakApiService.insertKontak(kontak)
+
+        // Mengembalikan nilai true jika penambahan berhasil, false sebaliknya
+        return response.isSuccessful
+    }
 }
