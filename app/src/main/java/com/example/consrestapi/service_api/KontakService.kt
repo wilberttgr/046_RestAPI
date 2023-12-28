495a49ef0e345686033b8e9c61b876f9926b6cae
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-28 18:19:55 +0700

    add suspend fun deleteKontak and insertKontak

--- app/src/main/java/com/example/consrestapi/service_api/KontakService.kt
+++ app/src/main/java/com/example/consrestapi/service_api/KontakService.kt
@@ -1,8 +1,13 @@
 package com.example.consrestapi.service_api
 
 import com.example.consrestapi.model.Kontak
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface KontakService {
 
@@ -11,4 +16,16 @@ interface KontakService {
     )
     @GET("/kontak")
     suspend fun getKontak(): List<Kontak>
+    @Headers(
+        "Accept: application/json"
+    )
+    @DELETE("/kontak/{id}")
+    suspend fun deleteKontak(@Path("id") id: Int): Response<Unit>
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+    @POST("/kontak")
+    suspend fun insertKontak(@Body kontak: Kontak): Response<Unit>
 }
