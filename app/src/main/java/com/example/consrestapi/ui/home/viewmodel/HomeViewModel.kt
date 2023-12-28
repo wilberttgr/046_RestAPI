fb061d664979a19150a64dfb80b142607a5cf82a
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-12-28 18:23:23 +0700

    add fun deleteKontak

--- app/src/main/java/com/example/consrestapi/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/consrestapi/ui/home/viewmodel/HomeViewModel.kt
@@ -37,4 +37,15 @@ class HomeViewModel (private val kontakRepository: KontakRepository) : ViewModel
             }
         }
     }
+    fun deleteKontak(id: Int){
+        viewModelScope.launch {
+            try {
+                kontakRepository.deleteKontak(id)
+            } catch (e: IOException){
+                KontakUIState.Error
+            }catch (e: IOException){
+                KontakUIState.Error
+            }
+        }
+    }
 }
